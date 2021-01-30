31885326e18e2ef48ed72267e90a158e99857c6f
NormanR20 <norman17.rm@gmail.com>
2021-01-30 14:05:53 -0600

    :hammer: trigger de bitacoras terminados

---
+++ documents/scripts-base-datos/querys-poa-pacc-insert-tipobitacora.sql
@@ -0,0 +1,7 @@
+-- -----------------------------------------------------------Tabla tipobitacora------------------------------------------------------------
+
+insert into `poa-pacc-bd`.`tipobitacora`(tipoBitacora,descripcion) values ('insert','Se ha insertado un nuevo dato');
+insert into `poa-pacc-bd`.`tipobitacora`(tipoBitacora,descripcion) values ('update','Se ha modificado un dato existente');
+insert into `poa-pacc-bd`.`tipobitacora`(tipoBitacora,descripcion) values ('delete','Se ha eliminado un dato existente');
+insert into `poa-pacc-bd`.`tipobitacora`(tipoBitacora,descripcion) values ('update','Se ha registrado un cambio en la sesion');
+insert into `poa-pacc-bd`.`tipobitacora`(tipoBitacora,descripcion) values ('update','Se ha registrado un cambio en credenciales');
