51151bf6057feaa134b1bb9dc73d8026dc9e6014
TaniaMGV <taniamgv93@hotmail.com>
2021-02-15 09:03:56 -0600

    🔒Añadiendo Edpoints pendientes
    Añadiendo Edpoint a modulos Departamentos y dimensiones Administrativas

--- backend/api/dimensiones-administrativas/listar-dimensiones.php
+++ backend/api/dimensiones-administrativas/listar-dimensiones.php
@@ -1,10 +1,19 @@
 <?php
     require_once('../request-headers.php');
+    require_once('../../middlewares/VerificarToken.php');
     require_once('../../controllers/DimensionesAdministrativasController.php');
     switch ($_SERVER['REQUEST_METHOD']) {
-        case 'POST': 
-            $dimension = new DimensionesAdministrativasController();
-            $dimension->listarDimensiones();
+        case 'POST':
+            $verificarTokenAcceso = new verificarTokenAcceso();
+            $tokenEsValido = $verificarTokenAcceso->verificarTokenAcceso();
+            if ($tokenEsValido) {
+                $dimension = new DimensionesAdministrativasController();
+                $dimension->listarDimensiones();
+            } else {
+                $dimension = new DimensionesAdministrativasController();
+                $dimension->peticionNoAutorizada();
+                require_once('../destruir-sesiones.php');
+            }
         break;
         default: 
             $dimension = new DimensionesAdministrativasController();
